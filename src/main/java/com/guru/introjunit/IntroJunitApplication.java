643db37eda1474cf5517525b8d72ab7343f38bbd
ahamedshaik16 <ahamedshaik16@gmail.com>
2021-07-15 07:28:02 +0530

    Initial Setup

---
+++ src/main/java/com/guru/introjunit/IntroJunitApplication.java
@@ -0,0 +1,13 @@
+package com.guru.introjunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroJunitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroJunitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guru/introjunit/IntroJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guru.introjunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroJunitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
